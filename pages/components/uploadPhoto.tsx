1efa0cad24071f2a1e0a69d47e09fda636ebddac
yankyhermawan <yankyhermawan42@gmail.com>
2023-07-14 20:59:47 +0700

    create uploadPhoto components

--- pages/components/uploadPhoto.tsx
+++ pages/components/uploadPhoto.tsx
@@ -1,3 +1,23 @@
+import { PickerOverlay, PickerOverlayProps } from "filestack-react";
+import { useState } from "react";
+
 export function UploadPhoto() {
-	return <></>;
+	const [display, setDisplay] = useState(false);
+	const displayButton = () => setDisplay(true);
+	return (
+		<>
+			<button onClick={displayButton}>Abc</button>
+			{display ? (
+				<PickerOverlay
+					apikey={"A1fRtRqdMQ2eueTZwM36Cz"}
+					onUploadDone={(result) => {
+						//@ts-ignore
+						console.log(result.filesUploaded[0].url);
+					}}
+				/>
+			) : (
+				<></>
+			)}
+		</>
+	);
 }
