cc75844386712ee4417491e0a494556370964935
yankyhermawan <yankyhermawan42@gmail.com>
2023-07-17 19:37:36 +0700

    finish contact element

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -2,82 +2,82 @@ import React, { useEffect } from "react";
 import Image from "next/image";
 
 const Login = () => {
-  const handleLogin = () => {
-    // Handle login logic here
-  };
+	const handleLogin = () => {
+		// Handle login logic here
+	};
 
-  const handleRegister = () => {
-    // Handle register logic here
-  };
+	const handleRegister = () => {
+		// Handle register logic here
+	};
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
+	useEffect(() => {
+		document.body.style.overflow = "hidden";
+		return () => {
+			document.body.style.overflow = "auto";
+		};
+	}, []);
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-yellow-200">
-      <div className="flex bg-white rounded-xl shadow items-center justify-center">
-        <div className="w-1/2 px-8 py-12">
-          <Image
-            src="/recipeslogo.png"
-            alt="Logo-login"
-            width={100}
-            height={100}
-            className="w-full h-full object-cover rounded-tr-xl rounded-br-xl"
-          />
-          <h1 className="text-2xl font-bold mb-4 text-center">
-            Welcome to Recipes!
-          </h1>
-          <form>
-            <div className="mb-4">
-              <label className="block mb-2">Email</label>
-              <input
-                className="w-full border border-gray-300 p-2 rounded-xl"
-                type="email"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-2">Password</label>
-              <input
-                className="w-full border border-gray-300 p-2 rounded-xl"
-                type="password"
-              />
-            </div>
-            <button
-              className="bg-yellow-200 hover:bg-yellow-300 text-black py-2 px-4 rounded-xl w-full shadow-xl font-bold"
-              type="button"
-              onClick={handleLogin}
-            >
-              Login
-            </button>
-          </form>
-          <p className="mt-4 text-sm">
-            Don't have an account?{" "}
-            <a
-              href="#"
-              onClick={handleRegister}
-              className="text-blue-500 hover:underline"
-            >
-              Register here
-            </a>
-          </p>
-        </div>
-        <div className="w-1/2 border-l">
-          <Image
-            src="/loginpage-img.jpg"
-            alt="Login"
-            width={500}
-            height={500}
-            objectFit="content"
-            className="w-full h-full object-cover rounded-tr-xl rounded-br-xl"
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="fixed inset-0 flex items-center justify-center bg-yellow-200 text-black">
+			<div className="flex bg-white rounded-xl shadow items-center justify-center">
+				<div className="w-1/2 px-8 py-12">
+					<Image
+						src="/recipeslogo.png"
+						alt="Logo-login"
+						width={350}
+						height={350}
+						className="h-full object-cover rounded-tr-xl rounded-br-xl m-auto"
+					/>
+					<h1 className="text-2xl font-bold mb-4 text-center">
+						Welcome to Recipes!
+					</h1>
+					<form>
+						<div className="mb-4">
+							<label className="block mb-2">Email</label>
+							<input
+								className="w-full border border-gray-300 p-2 rounded-xl"
+								type="email"
+							/>
+						</div>
+						<div className="mb-4">
+							<label className="block mb-2">Password</label>
+							<input
+								className="w-full border border-gray-300 p-2 rounded-xl"
+								type="password"
+							/>
+						</div>
+						<button
+							className="bg-yellow-200 hover:bg-yellow-300 text-black py-2 px-4 rounded-xl w-full shadow-xl font-bold"
+							type="button"
+							onClick={handleLogin}
+						>
+							Login
+						</button>
+					</form>
+					<p className="mt-4 text-sm">
+						Don&apos;t have an account?{" "}
+						<a
+							href="#"
+							onClick={handleRegister}
+							className="text-blue-500 hover:underline"
+						>
+							Register here
+						</a>
+					</p>
+				</div>
+				<div className="w-1/2 border-l">
+					<Image
+						src="/loginpage-img.jpg"
+						alt="Login"
+						width={500}
+						height={500}
+						objectFit="fill"
+						className="w-full h-full object-cover rounded-tr-xl rounded-br-xl"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- pages/components/Home.tsx
+++ pages/components/Home.tsx
@@ -15,20 +15,18 @@ const Navbar = ({ logo }: NavbarProps) => {
 		scroll.scrollToTop();
 	};
 
+	const [isLoginModalOpen, setLoginModalOpen] = useState(false);
 
-  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+	const openLoginModal = () => {
+		setLoginModalOpen(true);
+	};
 
-  const openLoginModal = () => {
-    setLoginModalOpen(true);
-  };
-
-  const closeLoginModal = () => {
-    setLoginModalOpen(false);
-  };
-
+	const closeLoginModal = () => {
+		setLoginModalOpen(false);
+	};
 
 	return (
-		<div className="bg-white p-10">
+		<div className="bg-white p-10 text-black">
 			<nav className="bg-yellow-200 h-screen rounded-xl">
 				<div className="flex items-center justify-between">
 					<div>
@@ -72,90 +70,128 @@ const Navbar = ({ logo }: NavbarProps) => {
 							</ScrollLink>
 						</li>
 
+						<li>
+							<a
+								onClick={openLoginModal}
+								className="cursor-pointer contact"
+								style={{
+									color: "#181823",
+									backgroundColor: "#F5F5F5",
+									display: "block",
+									padding: "8px",
+									border: "3px solid black",
+									boxShadow: "5px -5px 0 0px black",
+									position: "relative",
+									top: "0",
+									left: "0",
+									transition: "box-shadow 1s, left 1s, top 1s",
+								}}
+								onMouseEnter={(e) => {
+									e.currentTarget.style.boxShadow = "0 0 0 -3px white";
+									e.currentTarget.style.top = "-10px";
+									e.currentTarget.style.left = "10px";
+								}}
+								onMouseLeave={(e) => {
+									e.currentTarget.style.boxShadow = "5px -5px 0 0px black";
+									e.currentTarget.style.top = "0";
+									e.currentTarget.style.left = "0";
+								}}
+							>
+								Login
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="flex items-center justify-between p-10">
+					<div className="w-1/2">
+						<h1
+							className="py-2"
+							style={{
+								color: "#F5F5F5",
+								fontWeight: 900,
+								textTransform: "uppercase",
+								fontSize: "clamp(2rem, 6vw, 4rem)",
+								lineHeight: "0.85em",
+								textAlign: "center",
+								textShadow:
+									"3px 1px 1px #181823, 2px 2px 1px #165bfb, 4px 2px 1px #181823, 3px 3px 1px #165bfb, 5px 3px 1px #181823, 4px 4px 1px #165bfb, 6px 4px 1px #4af7ff, 5px 5px 1px #165bfb, 7px 5px 1px #4af7ff, 6px 6px 1px #165bfb, 8px 6px 1px #4af7ff, 7px 7px 1px #165bfb, 9px 7px 1px #4af7ff",
+							}}
+						>
+							<span data-text="give an access to a lot of">
+								give an access to a lot of recipes
+							</span>
+							<span data-text=". it 's not just an ordinary">
+								. it&apos;s not just an ordinary recipe
+							</span>
+							<span data-text=". it's sophisticated">
+								. it&apos;s sophisticated
+							</span>
+						</h1>
 
-            <li>
-              <a
-                onClick={openLoginModal}
-                className="cursor-pointer contact"
-                style={{
-                  color: "#181823",
-                  backgroundColor: "#F5F5F5",
-                  display: "block",
-                  padding: "8px",
-                  border: "3px solid black",
-                  boxShadow: "5px -5px 0 0px black",
-                  position: "relative",
-                  top: "0",
-                  left: "0",
-                  transition: "box-shadow 1s, left 1s, top 1s",
-                }}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.boxShadow = "0 0 0 -3px white";
-                  e.currentTarget.style.top = "-10px";
-                  e.currentTarget.style.left = "10px";
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.boxShadow = "5px -5px 0 0px black";
-                  e.currentTarget.style.top = "0";
-                  e.currentTarget.style.left = "0";
-                }}
-              >
-                Login
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex items-center justify-between p-10">
-          <div className="w-1/2">
-            <h1
-              className="py-2"
-              style={{
-                color: "#F5F5F5",
-                fontWeight: 900,
-                textTransform: "uppercase",
-                fontSize: "clamp(2rem, 6vw, 4rem)",
-                lineHeight: "0.85em",
-                textAlign: "center",
-                textShadow:
-                  "3px 1px 1px #181823, 2px 2px 1px #165bfb, 4px 2px 1px #181823, 3px 3px 1px #165bfb, 5px 3px 1px #181823, 4px 4px 1px #165bfb, 6px 4px 1px #4af7ff, 5px 5px 1px #165bfb, 7px 5px 1px #4af7ff, 6px 6px 1px #165bfb, 8px 6px 1px #4af7ff, 7px 7px 1px #165bfb, 9px 7px 1px #4af7ff",
-              }}
-            >
-              <span data-text="give an access to a lot of">
-                give an access to a lot of recipes
-              </span>
-              <span data-text=". it 's not just an ordinary">
-                . it 's not just an ordinary recipe
-              </span>
-              <span data-text=". it's sophisticated">. it's sophisticated</span>
-            </h1>
+						<p className="py-5 text-justify font-Lato font-bold">
+							&quot;Welcome to our Recipes Blog! Discover a world of culinary
+							excellence with our collection of professional-grade recipes. Join
+							us to explore a diverse range of dishes, from appetizers to
+							desserts, meticulously crafted by industry experts. Sign up today
+							to unlock exclusive content and personalized recommendations
+							tailored to your taste. Connect with a community of passionate
+							food enthusiasts, share your own recipes, and learn from
+							others.&quot;
+						</p>
+					</div>
+					<div className="flex items-center w-1/2">
+						<div className="justify-center">
+							<Image
+								src="/home-img.png"
+								alt="Home"
+								width={1000}
+								height={1000}
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="px-10">
+					<a
+						onClick={openLoginModal}
+						className="cursor-pointer contact"
+						style={{
+							color: "#181823",
+							backgroundColor: "#F5F5F5",
+							display: "block",
+							padding: "8px",
+							border: "3px solid black",
+							boxShadow: "5px -5px 0 0px black",
+							position: "relative",
+							top: "0",
+							left: "0",
+							transition: "box-shadow 1s, left 1s, top 1s",
+							width: "fit-content",
+						}}
+						onMouseEnter={(e) => {
+							e.currentTarget.style.boxShadow = "0 0 0 -3px white";
+							e.currentTarget.style.top = "-10px";
+							e.currentTarget.style.left = "10px";
+						}}
+						onMouseLeave={(e) => {
+							e.currentTarget.style.boxShadow = "5px -5px 0 0px black";
+							e.currentTarget.style.top = "0";
+							e.currentTarget.style.left = "0";
+						}}
+					>
+						Register
+					</a>
+				</div>
+			</nav>
 
-            <p className="py-5 text-justify font-Lato font-bold">
-              "Welcome to our Recipes Blog! Discover a world of culinary
-              excellence with our collection of professional-grade recipes. Join
-              us to explore a diverse range of dishes, from appetizers to
-              desserts, meticulously crafted by industry experts. Sign up today
-              to unlock exclusive content and personalized recommendations
-              tailored to your taste. Connect with a community of passionate
-              food enthusiasts, share your own recipes, and learn from others."
-            </p>
-          </div>
-          <div className="flex items-center w-1/2">
-            <div className="justify-center">
-              <img src="/home-img.png" alt="Home" width={1000} height={1000} />
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <Modal
-        isOpen={isLoginModalOpen}
-        onRequestClose={closeLoginModal}
-        contentLabel="Login Modal"
-      >
-        <Login />
-      </Modal>
-    </div>
-  );
+			<Modal
+				isOpen={isLoginModalOpen}
+				onRequestClose={closeLoginModal}
+				contentLabel="Login Modal"
+			>
+				<Login />
+			</Modal>
+		</div>
+	);
 };
 
 export default Navbar;
