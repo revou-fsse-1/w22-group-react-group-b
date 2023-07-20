4fc5c8ac9995dedffbd73acf2cda2cdc1cc492b5
yankyhermawan <yankyhermawan42@gmail.com>
2023-07-20 19:58:42 +0700

    Refactor recipe step variable name and fix handleChange function

--- pages/components/uploadPhoto.tsx
+++ pages/components/uploadPhoto.tsx
@@ -20,7 +20,7 @@ export default function UploadPhoto({
 			category: string;
 			url: string;
 			ingredients: string[];
-			steps: string[];
+			step: string[];
 		}>
 	>;
 }) {
--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -6,7 +6,7 @@ export default function NewRecipe() {
 		category: "",
 		url: "",
 		ingredients: ["", ""],
-		steps: ["", ""],
+		step: ["", ""],
 	});
 
 	const addItem = (e: React.SyntheticEvent, arrayName: string) => {
@@ -23,24 +23,68 @@ export default function NewRecipe() {
 		});
 	};
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+	) => {
 		const arrayName = e.target.name;
-		const index = e.target.id;
+		const index = +e.target.id;
 		setRecipe((prevRecipe) => {
-			const newItem = [...prevRecipe[arrayName as keyof typeof prevRecipe]];
-			newItem[+index] = e.target.value;
-			return { ...prevRecipe, newItem };
+			if (e.target.name === "category") {
+				const newValue = Array.isArray(e.target.value)
+					? e.target.value
+					: e.target.value;
+				return { ...prevRecipe, [arrayName]: newValue };
+			}
+			const newArray = [...prevRecipe[arrayName as keyof typeof prevRecipe]];
+			const newValue = Array.isArray(e.target.value)
+				? e.target.value
+				: e.target.value;
+			console.log(newValue);
+			newArray[index] = newValue;
+			return { ...prevRecipe, [arrayName]: newArray };
 		});
 	};
+
+	const handleStringChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setRecipe((prevRecipe) => ({
+			...prevRecipe,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const postButton = (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		console.log(recipe);
+	};
+	console.log(recipe);
 	return (
 		<form className="text-black">
+			<div className="border-solid border-black border-2">
+				<input
+					type="text"
+					name="name"
+					value={recipe.name}
+					onChange={handleStringChange}
+					placeholder="Nama Masakan"
+				/>
+			</div>
+			<div className="border-solid border-black border-2">
+				<select name="category" onChange={handleChange}>
+					<option value="Western">Western</option>
+					<option value="Asian">Asian</option>
+					<option value="Indonesian">Indonesian</option>
+					<option value="Vegetarian">Vegetarian</option>
+					<option value="Sweets">Sweets</option>
+				</select>
+			</div>
+
 			<UploadPhoto setRecipe={setRecipe} />
 			{recipe.ingredients.map((ing, index) => {
 				return (
 					<div key={index} className="border-solid border-black border-2">
 						<input
 							type="text"
-							name="ingredient"
+							name="ingredients"
 							placeholder="1/2 ekor kuda"
 							value={ing}
 							onChange={handleChange}
@@ -56,7 +100,7 @@ export default function NewRecipe() {
 			>
 				Tambah Bahan
 			</button>
-			{recipe.steps.map((step, index) => {
+			{recipe.step.map((step, index) => {
 				return (
 					<div key={index} className="border-solid border-black border-2">
 						<input
@@ -77,7 +121,7 @@ export default function NewRecipe() {
 			>
 				Tambah Langkah
 			</button>
-			<button>Simpan</button>
+			<button onClick={postButton}>Simpan</button>
 		</form>
 	);
 }
