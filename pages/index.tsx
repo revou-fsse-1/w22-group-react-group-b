0df14ed850763e9cb495c94270614aecac6f8fdc
Rio Y Kurniawan <rioyulinanda@gmail.com>
2023-07-22 13:45:57 +0800

    add readme and fix the styling on recipe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import logo from "../public/recipeslogo.png";
 
 const Index = () => {
-	return (
-		<div>
-			<Home logo={logo} />
-			<Contact />
-		</div>
-	);
+  return (
+    <div>
+      <Home logo={logo} />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default Index;
